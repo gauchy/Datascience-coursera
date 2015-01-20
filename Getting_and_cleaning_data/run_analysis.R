6bad45a80d5d4557d8db28c9db602c319ae22c0d
ashwini.rai <ashwini.rai@nsn.com>
2015-01-21 00:01:02 +0530

    adding run_analysis.R

---
+++ Getting_and_cleaning_data/run_analysis.R
@@ -0,0 +1,14 @@
+run_analysis <- function()
+{
+  #Extract mean and sd
+  
+  #merge all 3 data of test 
+  #merge all 3 data of training
+  
+  #merge test and training
+  
+  
+  #apply labels
+  
+  #Take averages
+}
