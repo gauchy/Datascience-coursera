c4e78d75d84cf37e0d3a0143c9118111279eafce
ashwini.rai <ashwini.rai@nsn.com>
2015-01-21 00:40:46 +0530

    reading data from files , ReadME.md modifications

---
+++ Getting_and_cleaning_data/ReadME.md
@@ -0,0 +1,3 @@
+
+Note : Set working directory to the repository directory.
+Extracted "UCI HAR Dataset" should be present in the cloned repository location.
--- Getting_and_cleaning_data/run_analysis.R
+++ Getting_and_cleaning_data/run_analysis.R
@@ -1,8 +1,21 @@
 run_analysis <- function()
 {
+  #read files
+  
+  colNames <- read.table("UCI HAR Dataset\\features.txt");
+  activityLableMap <- read.table("UCI HAR Dataset\\activity_labels.txt");
+  
+  test_readings <- read.table("UCI HAR Dataset\\test\\X_test.txt");
+  test_activity_labels <- read.table("UCI HAR Dataset\\test\\y_test.txt");
+  test_subjects <- read.table("UCI HAR Dataset\\test\\subject_test.txt");
+  
+  train_readings <- read.table("UCI HAR Dataset\\train\\X_train.txt");
+  train_activity_labels <- read.table("UCI HAR Dataset\\train\\y_train.txt");
+  train_subjects <- read.table("UCI HAR Dataset\\train\\subject_train.txt");
+  
   #Extract mean and sd
   
-  #merge all 3 data of test 
+  #merge all 3 data of test
   #merge all 3 data of training
   
   #merge test and training
