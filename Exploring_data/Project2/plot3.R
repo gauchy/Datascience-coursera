e94c5ba6ae19861a96acb6237d5f1e2773e0f980
ashwini.rai <ashwini.rai@nsn.com>
2015-02-22 11:54:16 +0530

    Plot3

---
+++ Exploring_data/Project2/plot3.R
@@ -0,0 +1,18 @@
+plot3 <- function()
+{
+  #library ggplot2 needed
+  NEI <- readRDS("summarySCC_PM25.rds");
+  SCC <- readRDS("Source_Classification_Code.rds");
+  BaltimoreEI <- NEI[NEI["fips"]==24510,]
+  
+  g <- ggplot(BaltimoreEI,aes(year,Emissions))
+  
+  #Removed outliers by setting the ylim to 300
+  g <- g + geom_point(alpha="0.2") + geom_smooth(method="lm",se=T) +facet_grid(.~type) + coord_cartesian(ylim=c(0,300));
+  print(g)
+  
+  dev.copy(png,file="plot3.png")
+  dev.off()
+
+  
+}
